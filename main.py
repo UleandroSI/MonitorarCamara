2dcb0868de6f972466b858ae5c5dec41d523c3b1
Leandro Barbosa <uleandrosp7@gmail.com>
2022-03-11 16:09:57 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = "uLeandroSP"
+__copyright__ = "uLeandroSI"
+__credits__ = ["https://github.com/pji110-2021grupo5/pji110-2021grupo5"]
+__license__ = "MIT Licence"
+__version__ = "1.0"
+__maintainer__ = "https://github.com/UleandroSI"
+__email__ = "uleandrosp7@gmail.com"
+__status__ = "Development"
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from webscrapping import coletar
+
+inicio = coletar()
